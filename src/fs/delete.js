2e67962e13aa55e9b5b88a0705711047d933a52c
web dev <rublin@gmx.de>
2022-12-04 09:34:17 +0100

    feat: implemented file delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+
 const remove = async () => {
-    // Write your code here 
+    try 
+    {
+        await fs.promises.access("src/fs/files/fileToRemove.txt");
+        await fs.promises.rm("src/fs/files/fileToRemove.txt");
+    }
+    catch (e) {
+        throw new Error("FS operation failed");
+    }
+    
 };
 
 await remove();
