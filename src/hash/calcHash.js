11822945203217dc317ba5f39f79ca8c24ac10a8
web dev <rublin@gmx.de>
2022-12-04 16:26:03 +0100

    feat: implemented calculate hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import * as crypto from "crypto";
+import { readFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const __filename = resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const data = await readFile(__filename);
+    return console.log(crypto.createHash("sha256").update(data).digest("hex"));
 };
 
 await calculateHash();
