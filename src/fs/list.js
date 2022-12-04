6fea3f4f047f966c5df3a289ce5a91870b8fc392
web dev <rublin@gmx.de>
2022-12-04 09:39:52 +0100

    feat: implemented list of files function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+    try 
+    {
+        const files = await fs.promises.readdir("src/fs/files", { withFileTypes: true });
+        for (let file of files) {
+            console.log(file.name);
+        }
+    }
+    catch (e) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await list();
