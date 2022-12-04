633fc734d0b573f97bbe15747193e8e1c16d0026
web dev <rublin@gmx.de>
2022-12-04 09:00:19 +0100

    feat: implemented file copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+    try 
+    {
+        const files = await fs.promises.readdir("src/fs/files", { withFileTypes: true });
+        await fs.promises.mkdir("src/fs/files_copy");
+        for (let file of files) {
+            await fs.promises.copyFile("src/fs/files/" + file.name, "src/fs/files_copy/" + file.name);
+        }
+    }
+    catch (e) {
+        throw new Error("FS operation failed");
+    }
 };
 
 copy();
