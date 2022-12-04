40df047df225a31e197b276930ab31c92ec9d34c
web dev <rublin@gmx.de>
2022-12-04 11:32:19 +0100

    feat: implemented function  that parses args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
+import { argv } from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+    const a = argv.slice(2);
+    let printLine = '';
+    for (let i = 0; i < a.length; i += 2) {
+        printLine += `${a[i].slice(2)} is ${a[i + 1]}, `;
+    }
+    console.log(printLine.substring(0, printLine.length - 2));
 };
 
 parseArgs();
