ce4bee72bfa69059d88f46fc88a1d34fa8546abe
web dev <rublin@gmx.de>
2022-12-04 09:28:58 +0100

    feat: implemented file rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+    try 
+    {
+        await fs.promises.access("src/fs/files/wrongFilename.txt");
+        try 
+        {
+            await fs.promises.access("src/fs/files/properFilename.md");
+        }
+        catch (e) {
+            await fs.promises.rename("src/fs/files/wrongFilename.txt", "src/fs/files/properFilename.md");
+        }
+    }
+    catch (e) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await rename();
