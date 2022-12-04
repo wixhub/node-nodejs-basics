6816dfbeacaf62721aa2a72951cc11aa57a24d8b
web dev <rublin@gmx.de>
2022-12-04 21:22:26 +0100

    feat: implemented workers functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,21 @@
+import os from 'os';
+import wt from 'worker_threads';
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const __filename = resolve(__dirname, 'worker.js');
+
+    function createWorker(number) {
+      return new Promise(result => {
+        const worker = new wt.Worker(__filename, { workerData: number });
+        worker.on('message', number => result({ status: 'resolved', data: number }));
+        worker.on('error', () => result({ status: 'error', data: null }));
+      });
+    }
+    
+    return console.log(await Promise.all(os.cpus().map(({}, i) => createWorker(10 + i))));
 };
 
 await performCalculations();
