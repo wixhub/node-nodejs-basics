28fd4abef3869ccbf64d12566ef9f90b3f22e184
web dev <rublin@gmx.de>
2022-12-04 16:36:49 +0100

    feat: implemented read stream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { stdout } from "process";
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const __filename = resolve(__dirname, 'files', 'fileToRead.txt');
+    const stream = createReadStream(__filename);
+    stream.pipe(stdout);
+    stream.on("readable", () => stream.read());
 };
 
 await read();
