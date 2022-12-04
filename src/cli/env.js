c570edb35dd735b9e1472df366f458a2031ed52f
web dev <rublin@gmx.de>
2022-12-04 10:58:53 +0100

    feat: implemented function  that parses env var

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,9 @@
 import { env } from "process";
 
 const parseEnv = () => {
-    Object.entries(env).forEach((entry) => {
-        if (entry[0].startsWith("RSS_")) {
-          console.log(`${entry[0]}=${entry[1]}`);
-        }
-      });
+    Object.entries(env).forEach((entry) => 
+      entry[0].startsWith("RSS_") ? console.log(`${entry[0]}=${entry[1]}`) : ''
+    );
 };
 
 parseEnv();
