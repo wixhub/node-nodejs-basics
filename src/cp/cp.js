f1bc861a0dfaf83128507103a959c4221392be32
web dev <rublin@gmx.de>
2022-12-05 08:50:45 +0100

    feat: implemented spawn function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import { spawn } from "child_process";
+import { stdin, stdout } from "process";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const __filename = resolve(__dirname, 'files', 'script.js');
+    const child = spawn("node", [__filename].concat(args));
+    stdin.pipe(child.stdin);
+    child.stdout.pipe(stdout);
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
