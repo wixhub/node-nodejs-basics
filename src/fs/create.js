0b7f4d8323b991177e434611c3ad0f28cc358ded
wixhub <rublin@gmx.de>
2023-06-17 20:41:04 +0200

    feat: use error msg

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,15 +3,10 @@ import fs from "fs";
 const create = async () => {
     try 
     {
-        await fs.promises.open("src/fs/files/fresh.txt");
-        throw new Error("FS operation failed");
+        await fs.promises.writeFile("src/fs/files/fresh.txt", "I am fresh and young",  { flag: 'wx' }, "utf-8");
     }
     catch (e) {
-        if(e != "Error: FS operation failed") {
-            await fs.promises.writeFile("src/fs/files/fresh.txt", "I am fresh and young", "utf-8");
-        } else {
-            throw e;
-        }
+        console.error("FS operation failed");
     }
 };
 
