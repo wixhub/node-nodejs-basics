3897acb76d7c0e54dd2efc68f31fdae2089eb808
wixhub <rublin@gmx.de>
2023-06-17 21:01:53 +0200

    feat: implemented read file function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+    try 
+    {
+        console.log(await fs.promises.readFile("src/fs/files/fileToRead.txt", { encoding: "utf-8" }));
+    }
+    catch (e) {
+        console.error("FS operation failed");
+    }
 };
 
 await read();
