bb8f15531e9782c7401dafe68c1acdc6962522e6
wixhub <rublin@gmx.de>
2023-06-17 21:52:43 +0200

    feat: implemented function that parses args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+import { argv } from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+    const a = argv.slice(2);
+    let printLine = '';
+    for (let i = 0; i < a.length; i += 2) {
+        printLine += `${a[i].slice(2)} is ${a[i + 1]}, `;
+    }
+    
+    printLine.length > 5 ?
+    console.log(printLine.substring(0, printLine.length - 2)) :
+    console.log("Try npm run cli:args");
 };
 
 parseArgs();
