6aeca03060ddfd844cae0e237bd47e25f662f224
wixhub <rublin@gmx.de>
2023-06-17 18:43:36 +0200

    feat: implemented file create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+    try 
+    {
+        await fs.promises.open("src/fs/files/fresh.txt");
+        throw new Error("FS operation failed");
+    }
+    catch (e) {
+        if(e != "Error: FS operation failed") {
+            await fs.promises.writeFile("src/fs/files/fresh.txt", "I am fresh and young", "utf-8");
+        } else {
+            throw e;
+        }
+    }
 };
 
 await create();
