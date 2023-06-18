526024e56a0fcd24ca41a3b534965c5ad21926f0
wixhub <rublin@gmx.de>
2023-06-18 07:55:16 +0200

    feat: implemented write stream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { stdin } from "process";
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const __filename = resolve(__dirname, 'files', 'fileToWrite.txt');
+    const stream = createWriteStream(__filename, { encoding: "utf-8" });
+    stdin.pipe(stream);
 };
 
 await write();
