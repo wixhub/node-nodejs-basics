39037dfc799c32c3175a1b0af9e94b794aefd9c6
wixhub <rublin@gmx.de>
2023-06-18 08:03:27 +0200

    feat: implemented compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createGzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import { promisify } from "util";
+import { pipeline } from "stream";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const __filename = resolve(__dirname, 'files', 'fileToCompress.txt');
+    const __archive = resolve(__dirname, 'files', 'archive.gz');
+    const source = createReadStream(__filename);
+    const destination = createWriteStream(__archive);
+    const pipe = promisify(pipeline);
+    const gzip = createGzip();
+    await pipe(source, gzip, destination);
 };
 
 await compress();
